90082fb2fffe66fe001d55730503ffd7abfc5def
BrandonTwede <twede.brandon@gmail.com>
2018-09-29 22:16:29 +0000

    Finished webpage

--- project2.js
+++ project2.js
@@ -1,16 +1,45 @@
-function getWord(){
+function getWord(part){
+    
+    //noun&hasDetails=typeOf
+    part = part.replace(/\[|\]/gm,'');
+    //let values = part.split(" ");
     let output = "";
-    var myurl= "https://wordsapiv1.p.mashape.com/words?random=true&partOfSpeech=noun&hasDetails=typeOf";
-    $.ajax({
+    var myurl= "https://wordsapiv1.p.mashape.com/words?random=true&partOfSpeech=" + part;// + "&hasDetails=typeOf";
+    if (part == "adverb"){
+        myurl += "&letterPattern=" + encodeURIComponent("^.+ly$");
+    } else if (part == "noun"){
+        myurl += "&hasDetails=typeOf";
+    }
+    
+    return $.ajax({
         url : myurl,
         crossDomain: true,
         headers: {
             "X-Mashape-Key":"6qhJCMXeFZmshhUaG5jal9G63yGQp1XaHIWjsnf6bEp1A2QeJm"
-        },
-        success : function(parsed_json) {
-            console.log(parsed_json);
-            $("#results").html(parsed_json["word"]);
         }
     });
+}
+
+function fillInStory(){
+    let regex = /(\[[\w\s]+\]*)/g, match;
+    let input = $("#story").val();
+    let deferreds = new Array();
+    while (match = regex.exec(input)){
+        let promise = getWord(match[0]);
+        let repl = match[0];
+        deferreds.push(promise);
+        promise.done(function(data) {
+            let temp = "<text title='"+data.results[0].definition+"'>" + data["word"] + "</text>";
+            input = input.replace(repl, temp);
+        }).fail(function(xhr) {
+            input = input.replace(repl, "ERROR");
+        });
+        
+        console.log(match);
+    }
+    
+    $.when(...deferreds).done(function (x){
+        $("#results").html(input);    
+    })
     
 }
